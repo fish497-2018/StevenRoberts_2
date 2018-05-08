959c366fc68d91db6130bb96f22cb92c3faed2fd
sr320 <roberts.sbr@gmail.com>
2018-05-08 13:44:37 -0700

    add comment to dplyr code

--- scripts/myscript.R
+++ scripts/myscript.R
@@ -2,6 +2,6 @@
 
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
-
+#this creates new table catergorizing fish by size
 fish_data_cat = fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
