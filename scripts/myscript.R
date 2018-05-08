fd667ca22546666b7ecb36ee9d193f36f6aacabe
sr320 <roberts.sbr@gmail.com>
2018-05-08 13:54:58 -0700

    mod table so scalelength < 1 removed

--- scripts/myscript.R
+++ scripts/myscript.R
@@ -4,4 +4,8 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 
 #this creates new table catergorizing fish by size
 fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 300, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "big", "small")) %>%
+  filter(scalelength > 1)
+
+
+fish_mod <- filter(fish_data_cat, scalelength > 1)
