9f2c8d58b6ab536e181e772aa24786dd067d645c
sr320 <roberts.sbr@gmail.com>
2018-05-08 13:57:06 -0700

    renamed data and revised script

--- scripts/myscript.R
+++ scripts/myscript.R
@@ -1,6 +1,6 @@
 
 
-fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
+fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 
 #this creates new table catergorizing fish by size
 fish_data_cat = fish_data %>%
